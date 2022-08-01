b84596e1b1ced9852bc2f13d51fd4916ee739704
bartu <bartu.oeztuerk@student.htw-berlin.de>
2022-08-01 21:31:00 +0200

    deleted entry config and added a new databse could not connect to the old one

--- src/main/java/com/example/demo/entry/EntryConfig.java
+++ src/main/java/com/example/demo/entry/EntryConfig.java
@@ -1,27 +1,27 @@
-package com.example.demo.entry;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
-
-@Configuration
-public class EntryConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner(EntryRepository repository){
-        return args -> {
-            Entry One = new Entry( "Test Title", "This is the Content of the first Entry of my diary", LocalDate.of(2002, Month.JULY, 26)
-            );
-            Entry Two = new Entry( "Test Title", "This is the Content of the second Entry of my diary", LocalDate.of(2002, Month.JULY, 28)
-            );
-
-            repository.saveAll(
-                    List.of(One,Two)
-            );
-        };
-    }
-}
+//package com.example.demo.entry;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.time.LocalDate;
+//import java.time.Month;
+//import java.util.List;
+//
+//@Configuration
+//public class EntryConfig {
+//
+//    @Bean
+//    CommandLineRunner commandLineRunner(EntryRepository repository){
+//        return args -> {
+//            Entry One = new Entry( "Test Title", "This is the Content of the first Entry of my diary", LocalDate.of(2002, Month.JULY, 26)
+//            );
+//            Entry Two = new Entry( "Test Title", "This is the Content of the second Entry of my diary", LocalDate.of(2002, Month.JULY, 28)
+//            );
+//
+//            repository.saveAll(
+//                    List.of(One,Two)
+//            );
+//        };
+//    }
+//}
